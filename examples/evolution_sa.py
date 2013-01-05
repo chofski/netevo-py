893977c3bb6dd27a16fed2a5585ed6bfdb9f4a91
Thomas Gorochowski <tom@chofski.co.uk>
2013-01-05 22:02:20 +0100

    Updates to SA evolution example

--- examples/evolution_sa.py
+++ examples/evolution_sa.py
@@ -2,8 +2,18 @@
 """
 evolution_sa.py
 
-Example
+This example introduces the concept of evolution using a simulated annealing
+metaheuristic. The user must provide functions that quantify a performance
+measure (smaller is better - like a cost) and a mutation that should be 
+performed to search for new network topologies. The performance function used
+here is based on the topological eigenratio measure that is calculated from
+the network topology and does not require simulation of the dynamics. Networks
+with reduced eigneratios are known to show improved synchronisation.
 """
+
+#=========================================
+# IMPORT THE LIBRARIES
+#=========================================
 
 # So that we can run the examples without netevo necessarily being 
 # in the system path.
@@ -17,24 +27,36 @@ import numpy as np
 import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 
+#=========================================
+# DEFINE MUTATIONS
+#=========================================
 
+# Define a function for searching for new networks
 def rewire (G):
 	n_to_rewire = int(random.expovariate(4.0))
 	if n_to_rewire < 1:
 		n_to_rewire = 1
 	netevo.random_rewire (G, n_to_rewire)
 
+#=========================================
+# DEFINE PERFORMANCE MEASURE
+#=========================================
 
+# Define a function for the performance measure (eigenratio: smaller = better)
 def eigenratio (G):
 	if nx.is_connected(G):
+		# Calculate the eigenrato (lambda_N/lambda_2)
 		L = nx.laplacian_matrix(G)
 		eigenvalues, eigenvectors = linalg.eig(L)
 		idx = eigenvalues.argsort()   
 		eigenvalues = eigenvalues[idx]
 		return eigenvalues[-1]/eigenvalues[1]
-
+	# The network is not connected -  it is not valid
 	return float('inf')
 
+#=========================================
+# CREATE THE NETWORK
+#=========================================
 
 # Create a random graph, but check that it is valid
 n = 100
@@ -43,11 +65,18 @@ while True:
 	G = nx.gnm_random_graph(n, 2*n)
 	if eigenratio(G) != float('inf'):
 		break
-	
+
+# No dynamics are required
 G.graph['node_dyn'] = False
 G.graph['edge_dyn'] = False
 
+#=========================================
+# EVOLVE THE NETWORK
+#=========================================
+
+# Perform the evolution
 iteration, G_final = netevo.evolve_sa (G, eigenratio, rewire, initial_temp=100.0, min_temp=0.0000001)
 
-netevo.write_gml(G, 'out_start.gml')
-netevo.write_gml(G_final, 'out_end.gml')
+# Output GML files containing the initial and final toplogies (viewable in Cytoscape/yEd)
+netevo.write_gml(G, 'evolution_sa_initial.gml')
+netevo.write_gml(G_final, 'evolution_sa_final.gml')
